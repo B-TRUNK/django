2557b2524a0c55b2d3ccd5d2b80784c5b9a8448f
abanoyb_boshra <abanoub.boshra.anis.com>
2025-10-20 09:25:40 +0300

    model_fields

--- product/models.py
+++ product/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+cats = [
+        ('Electronics' , 'Electronics'),
+        ('Clothing', 'Clothing'),
+        ('Other', 'Other'),
+        ]
+
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product/imgs/%Y/%m/%d/')
+    name = models.CharField(max_length=100, default='name' ,verbose_name='Product Name')
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    image = models.ImageField(upload_to='product/imgs/%Y/%m/%d/', default='product/imgs/default.jpg')
     active = models.BooleanField(default=True)
+    category = models.CharField(max_length=50, blank=True, null=True, choices=cats)
 
 
     def __str__(self):
