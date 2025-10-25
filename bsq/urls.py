47bb595a294330b3823e002bedafb19420bf83a4
abanoyb_boshra <abanoub.boshra.anis.com>
2025-10-25 13:30:27 +0300

    media_config

--- bsq/urls.py
+++ bsq/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mypages/' ,include('pages.urls')),
     path('lockers/' ,include('lockers.urls')),
     path('products/' ,include('product.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
