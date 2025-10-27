3684e13dca0f9cc89b33e27e16621c8bae8a0f7a
abanoyb_boshra <abanoub.boshra.anis.com>
2025-10-27 18:30:17 +0300

    forms_values

--- pages/forms.py
+++ pages/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+#label
+#initial
+#disabled
+#help_text
+#widget
+#required
+
+
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50)
-    password = forms.CharField(max_length=50)
+    username = forms.CharField(max_length=50, label='name', initial='user_name', required=True)
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput, required=True)
